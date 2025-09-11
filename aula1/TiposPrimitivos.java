4dd97216fc7354d6f77fa058b4a6015c22c07b1e
Paulo Campanha <paulocampanha@gmail.com>
2025-09-10 22:02:16 -0300

    Aula 1 adicionada

---
+++ aula1/TiposPrimitivos.java
@@ -0,0 +1,25 @@
+/* Nesse programa vamos fazer a introdução de Java trabalhando 
+ * com tipos primitivos da linguagem
+ * 
+ */
+package aula1;
+
+public class TiposPrimitivos {
+
+    public static void main(String args[]){
+
+        // Tipos primitivos para número inteiros
+        byte idade = 20;   // byte armazena números de -128 a 127
+        short capacidade = 30000; // short armazena números de -32.768 a 32.767
+        int premio = 1_500_000_000; // int armazena números de -2.147.483.648 a 2.147.483.647 
+        long populacao = 7_800_000_000_000L; //long armazena um número de -9.223.372.036.845.775.808 até 9.223.372.036.845.775.807  
+    }   int idade2 = 25;
+        long premio2 = 1_500_000_000L;  // L declara o cast do tipo de dados
+
+        // Tipos primitivos para números decimais (ponto flutuante)
+        float altura = 1.75f; // float armazena um número com até 7 casas depois da vírgula
+        double pi = 3.14159; // double armazena um número com até 15 casas depois da vírgula
+
+}
+
+
